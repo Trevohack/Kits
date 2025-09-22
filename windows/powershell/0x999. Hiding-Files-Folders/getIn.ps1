2ed35553e7d7cb850032566f43b7b2e5366b22dc
!0xTrev <136177431+Trevohack@users.noreply.github.com>
2025-09-22 22:48:31 +0530

    [Update] powershell

--- windows/powershell/0x999. Hiding-Files-Folders/getIn.ps1
+++ windows/powershell/0x999. Hiding-Files-Folders/getIn.ps1
@@ -1,6 +1,8 @@
 
-$FolderPath = "C:\SensitiveData"
+$FolderPath = "C:\data" 
 $Acl = Get-Acl $FolderPath
 $Ar = New-Object System.Security.AccessControl.FileSystemAccessRule("Everyone","ReadAndExecute","Deny")
 $Acl.AddAccessRule($Ar)
 Set-Acl $FolderPath $Acl
+
+
